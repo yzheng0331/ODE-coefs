364087a96c59de8f6b9bf8711440e06116d25813
yzheng0331 <yuxuanz@mit.edu>
2024-01-17 13:52:40 -0500

    update

--- GD/gd_randomized.py
+++ GD/gd_randomized.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
 import sys
+import random
 sys.path.append('../src') 
 from tqdm import tqdm
 from scipy.optimize import minimize
@@ -28,9 +29,40 @@ def cost_function(params, x, P980s):
     for P980, exp_blue in zip(P980_blue, experimental_data_blue):
         blue = compute_blue_for_x(x, P980, c1, c2, c3, c4, k31, k41, k51)
         cost_blue = (blue - exp_blue)**2
-        cost += cost_blue    
+        cost += cost_blue
 
     return cost / 10000
+
+def cost_randomized(params, x, P980s, coin):
+    c1, c2, c3, c4, k31, k41, k51 = params
+    c1, c2, c3, c4, k31, k41, k51 = 10**c1, 10**c2, 10**c3, 10**c4, 10**k31, 10**k41, 10**k51
+
+    P980_NIR, P980_blue = P980s
+    cost = 0
+    with open(f'./../synthetic_data/syn_{x}.json', 'r') as f:
+        loaded_json_data = f.read()
+    loaded_data = json.loads(loaded_json_data)
+    experimental_data_NIR = loaded_data.get('NIR', [])
+    experimental_data_blue = loaded_data.get('blue', [])
+
+    if coin < 1/2:
+        for P980, exp_nir in zip(P980_NIR, experimental_data_NIR):
+            nir = compute_NIR_for_x(x, P980, c1, c2, c3, c4, k31, k41, k51)
+            cost_NIR = (nir - exp_nir)**2
+            cost += cost_NIR
+    else:
+        for P980, exp_blue in zip(P980_blue, experimental_data_blue):
+            blue = compute_blue_for_x(x, P980, c1, c2, c3, c4, k31, k41, k51)
+            cost_blue = (blue - exp_blue)**2
+            cost += cost_blue
+
+    return cost / 500
+
+# grad[i] = grad_randomized(params_plus, params_minus, x, P980, epsilon)
+def grad_randomized(x_plus, x_minus, x, P980s, epsilon):
+    coin = random.uniform(0, 1)
+    return (cost_randomized(x_plus, x, P980s, coin) -
+            cost_randomized(x_minus, x, P980s, coin)) / (2 * epsilon)
 
 def gradient_descent(params, learning_rate, iterations, x, P980s):
     costs = []
@@ -58,6 +90,39 @@ def gradient_descent(params, learning_rate, iterations, x, P980s):
             learning_rate = min(learning_rate * 1.5, max_learning_rate)  
         params -= learning_rate * grad
         print(epoch, grad, learning_rate * grad, params, learning_rate)
+
+        current_cost = cost_function(params, x, P980s)
+        costs.append(current_cost)
+    
+    return params, costs
+
+def gradient_descent_rand(params, learning_rate, iterations, x, P980s):
+    costs = []
+
+    for epoch in tqdm(range(iterations)):
+        grad = np.zeros_like(params)
+        epsilon = 1e-2 # something to change
+        
+        rand = random.uniform(0, 1)
+        thre = 1/2
+
+        for i in range(len(params)):
+            params_plus = params.copy()
+            params_minus = params.copy()
+            params_plus[i] += epsilon
+            params_minus[i] -= epsilon
+            if rand < thre:
+                grad[i] = (cost_function(params_plus, x, P980s) -
+                        cost_function(params_minus, x, P980s)) / (2 * epsilon)
+            else:
+                grad[i] = grad_randomized(params_plus, params_minus, x, P980s, epsilon)
+        
+        max_learning_rate = 5e-6
+        if np.linalg.norm(grad) < 1e4:
+            learning_rate = min(learning_rate * 1.5, max_learning_rate)  
+        params -= learning_rate * grad
+        # print(epoch, grad, learning_rate * grad, params, learning_rate)
+        # print(rand)
 
         current_cost = cost_function(params, x, P980s)
         costs.append(current_cost)
@@ -109,14 +174,14 @@ def gradient_descent_adam(params, learning_rate, iterations, x, P980s):
     return params, costs
 
 
-conc = 10 ## Something to change
+conc = 15 ## Something to change
 #  2.25551330653112, -1.3056691656198742, 0.4984769347191149, 2.31942225639846, -0.631828119307082, 2.7201431777574223, -0.005981397391665376
 initial_params = [2, -1, 0, 2, 0, 2, 0] ## Something to change
 
 learning_rate = 5e-7  ##5e-3## 5e-7 ## Something to change
-iterations = 40 ## Something to change
+iterations = 100 ## Something to change
 P980s = power[conc]
-optimized_params, costs = gradient_descent(initial_params, learning_rate, iterations, conc, P980s)
+optimized_params, costs = gradient_descent_rand(initial_params, learning_rate, iterations, conc, P980s)
 
 print(optimized_params)
 print(costs)
